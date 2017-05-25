c3106cad3e1790fbb0985118a0a31b843b3c4f1b
matthieudou <matthieu.doultremont@gmail.com>
2017-05-25 09:29:22 +0200

    rails restaurants last commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+ratings = [0, 1, 2, 3, 4, 5]
+30.times do
+  resto = Restaurant.create(
+    name: Faker::Commerce.department,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: categories.sample
+    )
+  10.times do
+    review = Review.new(
+      content: Faker::Lorem.sentence,
+      rating: ratings.sample
+      )
+    review.restaurant = resto
+    review.save
+  end
+end
